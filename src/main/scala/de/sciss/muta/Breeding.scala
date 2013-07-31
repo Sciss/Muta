689c6a5ea3ba91ba9000cfa80c73583b2b4468de
Sciss <contact@sciss.de>
2013-07-31 18:55:00 +0200

    ...

--- src/main/scala/de/sciss/muta/Breeding.scala
+++ src/main/scala/de/sciss/muta/Breeding.scala
@@ -1,3 +1,31 @@
 package de.sciss.muta
 
-trait Breeding[S <: Sys[S]] extends ((S#GenomeSel, S#Global, util.Random) => S#Genome)
+// trait Breeding[S <: Sys[S]] extends ((S#GenomeSel, S#Global, util.Random) => S#Genome)
+
+case class Breeding[S <: Sys[S]](elitism        : SelectionSize     = SelectionNumber(5),
+                                 crossoverWeight: SelectionPercent  = SelectionPercent(80),
+                                 crossover      : BreedingFunction[S],
+                                 mutation       : BreedingFunction[S])
+  extends ((S#GenomeSel, S#Global, util.Random) => S#Genome) {
+
+  override def apply(g: S#GenomeSel, global: S#Global, r: util.Random): S#Genome = {
+    val szOut = g.size
+    val szEl  = elitism(szOut)
+    val out1  = if (szEl == 0) Vec.empty else {
+      // ensure that elite choices are distinct (don't want to accumulate five identical chromosomes over time)!
+      val eliteCandidates = g.map { case (c, f, _) => (c, f) } .distinct.sortBy(-_._2).map(_._1)
+      eliteCandidates.take(szEl)
+    }
+    val szBr  = szOut - out1.size
+    val szX   = crossoverWeight(szBr)
+    val szMut = szBr - szX
+    val sel   = g.collect {
+      case (c, _, true) => c
+    }
+    val out2  = if (szX   == 0) out1 else out1 ++ crossover(sel, szX  , global, r)
+    val out3  = if (szMut == 0) out2 else out2 ++ mutation (sel, szMut, global, r)
+    out3
+  }
+}
+
+trait BreedingFunction[S <: Sys[S]] extends ((S#Genome, Int, S#Global, util.Random) => S#Genome)
--- src/test/scala/de/sciss/muta/TestSys.scala
+++ src/test/scala/de/sciss/muta/TestSys.scala
@@ -13,7 +13,7 @@ class TestSys extends Sys[TestSys] {
 
   def defaultSelection: Selection[S] = SelectTrunc()
 
-  def defaultBreeding: Breeding[S] = ???
+  def defaultBreeding: Breeding[S] = Breeding(crossover = CrossoverImpl, mutation = MutationImpl)
 
   val chromosomeClassTag  = reflect.classTag[Chromosome]
   val selfTypeTag         = ru     .typeTag [TestSys   ]
@@ -28,5 +28,28 @@ case class SelectTrunc(size: SelectionSize = SelectionPercent()) extends Selecti
     val n       = size(pop.size)
     val sorted  = pop.sortBy(_._2)
     sorted.takeRight(n).map(_._1)
+  }
+}
+
+object CrossoverImpl extends BreedingFunction[TestSys] {
+  def apply(gen: TestSys#Genome, num: Int, glob: TestSys#Global, rand: util.Random): TestSys#Genome = Vec.fill(num) {
+    val i   = rand.nextInt(gen.size)
+    val j   = rand.nextInt(gen.size)
+    val gi  = gen(i)
+    val gj  = gen(j)
+    val x   = rand.nextInt(math.min(gi.size, gj.size))
+    gi.take(x) ++ gj.drop(x)
+  }
+}
+
+object MutationImpl extends BreedingFunction[TestSys] {
+  def apply(gen: TestSys#Genome, num: Int, glob: TestSys#Global, rand: util.Random): TestSys#Genome = Vec.fill(num) {
+    val i   = rand.nextInt(gen.size)
+    val gi  = gen(i)
+    val mut = rand.nextInt(gi.size / 10 + 1 )
+    (gi /: (1 to mut)) { (gj, _) =>
+      val j = rand.nextInt(gj.size)
+      gj.updated(j, !gj(j))
+    }
   }
 }
